67fc84cda5716d7db780465f3cfe517ebbb1b3e3
andzega2608 <150101221+andzega2608@users.noreply.github.com>
2024-10-30 18:18:48 -0600

    Update sorts.h

--- 2_Algoritmos_Busqueda_Ordenamiento/sorts.h
+++ 2_Algoritmos_Busqueda_Ordenamiento/sorts.h
@@ -1,9 +1,8 @@
 /*
- * Tarea #2
- *
- * Fecha: 29/08/2024
- * Autor: Andrés Ignacio Zegales Taborga
- * Matricula: A01711365
+ *      Tarea #2
+ * 	Fecha: 29/08/2024
+ *      Autor: Andrés Ignacio Zegales Taborga
+ *      Matricula: A01711365
  */
 
 #ifndef SORTS_H_
@@ -27,14 +26,30 @@ public:
 	int busqSecuencial(std::vector<T>&, int);
 	int busqBinaria(std::vector<T>&, int);
 };
-
+/*
+* swap(std::vector<T> &v, int i, int j)
+*
+* Realiza un cambio de posición entre dos
+* elementos dentro del vector
+*
+* param vector, posición i, posición j
+* @return
+*/
 template <class T>
 void Sorts<T>::swap(std::vector<T> &v, int i, int j) {
 	T aux = v[i];
 	v[i] = v[j];
 	v[j] = aux;
 }
-
+/*
+* ordenaBurbuja(std::vector<T> &v)
+*
+* Ordena el vector con el método burbuja
+* usando el método swap
+*
+* param vector a ordenar
+* @return
+*/
 template <class T>
 void Sorts<T>::ordenaBurbuja(std::vector<T> &v) {
     for (int i = 0; i < v.size(); i++){
@@ -45,7 +60,15 @@ void Sorts<T>::ordenaBurbuja(std::vector<T> &v) {
         }
     }
 }
-
+/*
+* ordenaSeleccion(std::vector<T> &v)
+*
+* Ordena el vector con el método selección
+* usando el método swap
+*
+* param vector a ordenar
+* @return
+*/
 template <class T>
 void Sorts<T>::ordenaSeleccion(std::vector<T> &v) {
 	int index;
@@ -61,16 +84,33 @@ void Sorts<T>::ordenaSeleccion(std::vector<T> &v) {
         }
 	}
 }
-
+/*
+* copyArray(std::vector<T> &A, std::vector<T> &B, int low, int high)
+*
+* Copia los elementos de un array en otro
+*
+* param std::vector<T> &A, std::vector<T> &B, int low, int high
+* @return
+*/
 template <class T>
-void Sorts<T>::copyArray(std::vector<T> &A, std::vector<T> &B, int low, int high) {
+void Sorts<T>::copyArray(std::vector<T> &A, std::vector<T> &B,
+                         int low, int high) {
     for (int i = low; i <= high; i++){
         A[i] = B[i];
     }
 }
-
+/*
+* mergeArray(std::vector<T> &A, std::vector<T> &B, int low, int mid, int high)
+*
+* Se encarga de ordenar el array, basicamente hace el
+* procedimiento del metodo Merge
+*
+* param std::vector<T> &A, std::vector<T> &B, int low, int mid, int high
+* @return
+*/
 template <class T>
-void Sorts<T>::mergeArray(std::vector<T> &A, std::vector<T> &B, int low, int mid, int high) {
+void Sorts<T>::mergeArray(std::vector<T> &A, std::vector<T> &B, int low,
+                          int mid, int high) {
 	int i, j, k;
 	i = low;
 	j = mid + 1;
@@ -95,9 +135,18 @@ void Sorts<T>::mergeArray(std::vector<T> &A, std::vector<T> &B, int low, int mid
 		}
 	}
 }
-
+/*
+* mergeSplit(std::vector<T> &A, std::vector<T> &B, int low, int high)
+*
+* Se encarga de dividir la lista en pequeñas partes, para luego
+* hacer el merge, ordenamiento correspondiente
+*
+* param std::vector<T> &A, std::vector<T> &B, int low, int high
+* @return
+*/
 template <class T>
-void Sorts<T>::mergeSplit(std::vector<T> &A, std::vector<T> &B, int low, int high) {
+void Sorts<T>::mergeSplit(std::vector<T> &A, std::vector<T> &B,
+                          int low, int high) {
 	int mid;
 	if ( (high - low) < 1 ) {
 		return;
@@ -108,17 +157,33 @@ void Sorts<T>::mergeSplit(std::vector<T> &A, std::vector<T> &B, int low, int hig
 	mergeArray(A, B, low, mid, high);
 	copyArray(A, B, low, high);
 }
-
+/*
+* ordenaMerge(std::vector<T> &v)
+*
+* Integra los dos metodos anteriores, a una manera mucho mas
+* adecuada para el usuario
+*
+* param std::vector<T> &v
+* return array ya ordenada
+*/
 template <class T>
 void Sorts<T>::ordenaMerge(std::vector<T> &v) {
 	std::vector<T> tmp(v.size());
 	mergeSplit(v, tmp, 0, v.size() - 1);
 }
-
-
+/*
+* busqSecuencial(std::vector<T> &v, int val)
+*
+* Realiza una busqueda elemento por elemento
+* con un ciclo for
+*
+* param std::vector<T> &v, int val
+* return posicion del elemento encontrado
+* si no se encuentra retorna -1
+*/
 template <class T>
 int Sorts<T>::busqSecuencial(std::vector<T> &v, int val) {
-	ordenaBurbuja(v); // Precondicion. Se asegura que el vector este ordenado
+	ordenaBurbuja(v); // Precondición. Se asegura que el vector este ordenado
 	for (int i = 0; i < v.size(); i++){
         if (val == v[i]){
             return i;
@@ -126,7 +191,16 @@ int Sorts<T>::busqSecuencial(std::vector<T> &v, int val) {
 	}
 	return -1;
 }
-
+/*
+* busqAux(std::vector<T> &v, int low, int high, int val)
+*
+* Realiza una busqueda elemento dividiendo el arreglo
+* y lo realiza de manera recursiva
+*
+* param std::vector<T> &v, int low, int high, int val
+* return posicion del elemento encontrado
+* si no se encuentra retorna -1
+*/
 template <class T>
 int Sorts<T>::busqAux(std::vector<T> &v, int low, int high, int val) {
 	int mid = (high + low) / 2;
@@ -145,10 +219,18 @@ int Sorts<T>::busqAux(std::vector<T> &v, int low, int high, int val) {
 	}
 	return -1;
 }
-
+/*
+* busqBinaria(std::vector<T> &v, int val)
+*
+* Método más fácil para el usuario
+*
+* param std::vector<T> &v, int val
+* return posicion del elemento encontrado
+* si no se encuentra retorna -1
+*/
 template <class T>
 int Sorts<T>::busqBinaria(std::vector<T> &v, int val) { // Funcion adecuada
-	ordenaBurbuja(v); // Precondicion. Se asegura que el vector este ordenado
+	ordenaBurbuja(v); // Precondición. Se asegura que el vector este ordenado
 	return busqAux(v, 0, v.size()-1, val);
 }
 #endif /* SORTS_H_ */
