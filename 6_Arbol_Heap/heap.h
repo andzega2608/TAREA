22962b254eeba6115b364625e51efdaaeb6431d0
andzega2608 <150101221+andzega2608@users.noreply.github.com>
2024-10-30 21:09:31 -0600

    Update heap.h

--- 6_Arbol_Heap/heap.h
+++ 6_Arbol_Heap/heap.h
@@ -36,14 +36,14 @@ class Heap {
         void clear();
         std::string toString() const;
     };
-
+// Constructor por default
 template <class T>
 Heap<T>::Heap(unsigned int sze) {
     size_heap = sze;
     data = new T[size_heap];
     count = 0;
 }
-
+// Destructor por default
 template <class T>
 Heap<T>::~Heap() {
     delete [] data;
@@ -51,39 +51,93 @@ Heap<T>::~Heap() {
     size_heap = 0;
     count = 0;
 }
-
+/*
+* empty() const
+*
+* Revisa si el heap esta vacio
+*
+* @param
+* return true o false
+*/
 template <class T>
 bool Heap<T>::empty() const {
     return (count == 0);
 }
-
+/*
+* full() const
+*
+* Revisa si el heap esta lleno
+*
+* @param
+* return true o false
+*/
 template <class T>
 bool Heap<T>::full() const {
     return (count == size_heap);
 }
-
+/*
+* parent(unsigned int pos) const
+*
+* Calcula y retorna la posición del nodo padre en el heap
+* dada una posición de nodo.
+*
+* param posicion
+* return posicion del padre
+*/
 template <class T>
 unsigned int Heap<T>::parent(unsigned int pos) const {
 	return (pos - 1) / 2;
 }
-
+/*
+* left(unsigned int pos) const
+*
+* Calcula y retorna la posición del hijo izquierdo
+* en el heap dada una posición de nodo.
+*
+* param posicion
+* return posicion del hijo izquierdo
+*/
 template <class T>
 unsigned int Heap<T>::left(unsigned int pos) const {
 	return (2 * pos) + 1;
 }
-
+/*
+* right(unsigned int pos) const
+*
+* Calcula y retorna la posición del hijo derecho
+* en el heap dada una posición de nodo.
+*
+* param posicion
+* return posicion del hijo derecho
+*/
 template <class T>
 unsigned int Heap<T>::right(unsigned int pos) const {
 	return (2 * pos) + 2;
 }
-
+/*
+* swap(unsigned int i, unsigned int j)
+*
+* Cambia la posición entre dos elementos dentro del heap
+*
+* param unsigned int i, unsigned int j
+* @return
+*/
 template <class T>
 void Heap<T>::swap(unsigned int i, unsigned int j) {
 	T aux = data[i];
 	data[i] = data[j];
 	data[j] = aux;
 }
-
+/*
+* heapify(unsigned int pos)
+*
+* Mantiene la propiedad del min-heap a partir de la
+* posición especificada. Se encarga de comparar el valor
+* de los hijos con el padre y realiza intercambios
+*
+* param unsigned int pos posicion del nodo a verificar
+* @return
+*/
 template <class T>
 void Heap<T>::heapify(unsigned int pos) {
     unsigned int le = left(pos);
@@ -100,7 +154,15 @@ void Heap<T>::heapify(unsigned int pos) {
 		heapify(min);
 	}
 }
-
+/*
+* push(T val)
+*
+* Inserta un nuevo valor en el heap, manteniendo
+* la estructura completa
+*
+* param T val
+* @return
+*/
 template <class T>
 void Heap<T>::push(T val)  {
 	unsigned int pos;
@@ -112,7 +174,16 @@ void Heap<T>::push(T val)  {
 	}
 	data[pos] = val;
 }
-
+/*
+* pop()
+*
+* Elimina y retorna el valor mínimo del heap
+* Coloca el último elemento del heap en la raíz
+* y luego aplica `heapify`
+*
+* @param
+* return T val, valor mínimo dentro del heap
+*/
 template <class T>
 T Heap<T>::pop() {
 	T aux = data[0];
@@ -120,23 +191,52 @@ T Heap<T>::pop() {
 	heapify(0);
 	return aux;
 }
-
+/*
+* top()
+*
+* Retorna el valor que se encuentra en la raiz,
+* es decir, el valor mínimo
+*
+* @param
+* return T val, valor mínimo/raíz del heap
+*/
 template <class T>
 T Heap<T>::top() {
 	T aux = data[0];
 	return aux;
 }
-
+/*
+* size() const
+*
+* Retornar el tamaño del heap
+*
+* @param
+* return tamaño del heap
+*/
 template <class T>
 int Heap<T>::size() const {
     return count;
 }
-
+/*
+* clear()
+*
+* Limpia todo el heap
+*
+* @param
+* @return
+*/
 template <class T>
 void Heap<T>::clear() {
     count = 0;
 }
-
+/*
+* toString() const
+*
+* Concatena los valores del heap en un string
+*
+* @param
+* return string con los valores concatenados
+*/
 template <class T>
 std::string Heap<T>::toString() const {
 	std::stringstream aux;
@@ -150,4 +250,5 @@ std::string Heap<T>::toString() const {
 	aux << "]";
 	return aux.str();
 }
+
 #endif /* HEAP_H_ */
